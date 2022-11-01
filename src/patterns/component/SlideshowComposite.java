3de3e36302df117a8a623b62b1fe4d3f86a02b3b
AmrietJainandunsing <amriet.jainandunsing@visma.com>
2022-11-01 15:40:05 +0100

    laatste fix?

--- src/patterns/component/SlideshowComposite.java
+++ src/patterns/component/SlideshowComposite.java
@@ -1,16 +1,10 @@
 package patterns.component;
 import patterns.factory.Prototype;
-import patterns.strategy.DrawStrategy;
 
 public class SlideshowComposite extends Composite implements Prototype<SlideshowComposite> {
-	private DrawStrategy strategy;
 	
 	@Override
 	public SlideshowComposite copy() {
 		return new SlideshowComposite();
-	}
-	
-	public void setStrategy(DrawStrategy strategy) {
-		this.strategy = strategy;
 	}
 }
